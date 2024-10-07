3408749558369013f8301f867bbaf43b312ffb93
Rajesh Singh <rajeshnksingh@gmail.com>
2024-10-07 12:38:53 +0530

    Multi Threading in java

---
+++ Day9/labs/second/SynchronizedWithClassImplemetedSync.java
@@ -0,0 +1,30 @@
+package Day9.labs.second;
+
+public class SynchronizedExample2 {
+    public static void main(String[] args) {
+        SyncCounter c = new SyncCounter();
+        new Thread(()->{
+            for (int i = 0; i < 50; i++) {
+                c.incerement();
+                System.out.println("Thread-1 " + Thread.currentThread().getName() + " " + c.getCount());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        new Thread(()->{
+            for (int i = 0; i < 50; i++) {
+                c.incerement();
+                System.out.println("Thread-2 " + Thread.currentThread().getName() + " " + c.getCount());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+}
